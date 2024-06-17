13ecc22c6dbedeedeb99341190452b9314f43bbe
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-17 15:25:07 +0000

    tasks 1 ahora si betty

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
- * print_alphabet - Entry point
- * Return: always 0.
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 
 void print_alphabet(void)

a82ea002c51ea5e35804ae064415fdb05f970f9c
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-18 15:30:44 +0000

    tasks 2 intento 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplica 2 int.
+ * @a: primer int.
+ * @b: Segundo int.
+ *
+ * Return: The result of the multiplication.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

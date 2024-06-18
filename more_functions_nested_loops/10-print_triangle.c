4f86903610f75b55bb064731a646c275888c2d4c
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-18 17:58:15 +0000

    me pase el infierno

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a triangle, using the character #.
+ * @size: The size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int h, i;
+
+	if (size > 0)
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (i = size - h; i > 0; i--)
+				_putchar(' ');
+
+			for (i = 0; i < h; i++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}

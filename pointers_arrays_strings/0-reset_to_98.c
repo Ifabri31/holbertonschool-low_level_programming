be50ea766c0bf9e642fcb050b4f95a3fbd80c89e
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-19 17:14:25 +0000

    Tasks 0 intento 1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * rest_to_98 - cambia el valor del int a 98
+ * @n: pointer
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Proyecto: C - Pointers, arrays and strings
+

15351bd81a9e2e925a149f4badeaa8037b59a2db
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-19 17:48:38 +0000

    tasks 1 intento 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int: cambia los valores de a y b entre ellos
+ * @a: pointer del primer value
+ * @b: pointer del segundo value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}

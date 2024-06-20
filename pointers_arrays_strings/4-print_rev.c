bf5475c4b1e34480366a31a1e72d1504a24af7be
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-20 15:13:45 +0000

    tasks 4 intento 1

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	while (*s == '\0')
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}

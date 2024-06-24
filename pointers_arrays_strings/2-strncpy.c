cd91e0fa2d87c87bb111eaafebb9eb0050b98a9d
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-24 18:01:36 +0000

    tasks 2 intento 1

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * _strncpy - copies a string
+ * _strncpy - copies strings
  * @dest: string destino
  * @src: string fuente
  * @n: number of character
+ *
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)

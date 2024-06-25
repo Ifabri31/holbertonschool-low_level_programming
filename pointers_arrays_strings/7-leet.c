36195cef632371341bbd920d0021025b979db7bf
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-25 20:49:01 +0000

    task 7 tapiaaaaa

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * leet - encode string into 1337
+ * @s: string to be encoded
+ *
+ * Return: string encoded
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+		}
+	}
+	return (s);
+}

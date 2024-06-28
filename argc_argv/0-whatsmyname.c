a89ae47cd3cb77868e91512db7883cd6b51fc962
Fabri <oviedofabrizzio1998@gmail.com>
2024-06-28 17:09:50 +0000

    task 0 intento 2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - prints de name
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }

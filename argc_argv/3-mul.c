3b5d357fc46708edccce20ef6a74bd1b4c147f5d
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-01 00:45:41 +0000

    tasks del domingo cansado

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -4,14 +4,14 @@
  * main - multiplies two numbers
  * @argc: argc
  * @argv: argv
- * 
+ *
  * Return: 0 success, 1
  */
 
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -21,6 +21,6 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
-	
+
 	return (0);
 }

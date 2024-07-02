e79d3a8aff3de43391ad7b73c4e8ae9557f41e7f
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-02 13:56:51 +0000

    task 1 ayudaa

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,2 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _strdup - return pointer to a new string
+ * @str: string
+ *
+ * Return: null if is null
+ */
+
+char *_strdup(char *str)
+{
+	char *c;
+	unsigned int lon, i;
+
+	if (str == NULL)
+		return (NULL);
+	for (lon = 0; str[lon] != '\0'; lon++)
+		;
+
+	c = malloc((lon + 1) * sizeof(char));
+	if (c == NULL)
+		return (NULL);
+
+	for (i = 0; i < lon; i++)
+		c[i] = str[i];
+	c[lon] = '\0';
+	return (c);
+}

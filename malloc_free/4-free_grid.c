76c0f4091eb890b3b9a011a1971c368f15561a84
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-05 15:30:11 +0000

    task 4 facilito

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees grid created by alloc_grid
+ * @grid: pointer to the array
+ * @height: the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	free(grid);
+}

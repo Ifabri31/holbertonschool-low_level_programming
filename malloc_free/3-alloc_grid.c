e38024a00e55b15a9a774b425901efe6b7c5d3cd
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-05 15:17:46 +0000

    task 3 casi casi?

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - 
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, ii;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (ii = 0; ii < i; ii++)
+			{
+				free(grid[ii]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (ii = 0; ii < width; ii++)
+		{
+			grid[i][ii] = 0;
+		}
+	}
+	return (grid);
+}

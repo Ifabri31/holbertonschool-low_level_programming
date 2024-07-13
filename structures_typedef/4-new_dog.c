72f261606a8d6c276c7fe6b9760befc0cd533b86
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-13 14:02:13 +0000

    task 4 el nuevo perrito

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * _strlen - return the length of a string
+ * @s: puntero a un char
+ *
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+
+	return (len);
+}
+/**
+ * *_strcpy - copia una cadena en otra
+ * @dest: cadena primera
+ * @src: cadena segunda
+ *
+ * Return: pointer dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - perro nuevo
+ * @name: nombre
+ * @age: edad
+ * @owner: dueÃo
+ *
+ * Return: new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -11,20 +58,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = malloc(strlen(name) + 1);
+	new_dog->name = malloc(_strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(new_dog->name, name);
-	new_dog->owner = malloc(strlen(owner) + 1);
+	_strcpy(new_dog->name, name);
+	new_dog->owner = malloc(_strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(new_dog->owner, owner);
+	_strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 	return (new_dog);
 }

7964352bd3ee65cebbaaf10da13bf35f88d5902b
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-31 18:07:40 +0000

    proyecto 1 task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h != NULL)
+	{
+		a++;
+		if (h->str == 0)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return(a);
+}

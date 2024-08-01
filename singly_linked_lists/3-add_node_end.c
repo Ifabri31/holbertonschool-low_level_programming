8423d448240631b6c80771433f43d53d0eb43052
fabri <oviedofabrizzio1998@gmail.com>
2024-08-01 16:46:10 +0000

    intento 2 tarea 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -34,11 +34,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (*head == NULL)
 		*head = new_node;
-
-	last_node = *head;
-	while (last_node->next != NULL)
-		last_node = last_node->next;
-	last_node->next = new_node;
-
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new_node;
+	}
 	return (new_node);
 }
